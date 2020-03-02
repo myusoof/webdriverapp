ffa4bf8cfb3f5fcb98961bcbf608c7a6cba27c2d
Mohmed Yusoof A.S <mohmedyusoofa.s@Rajeshs-MacBook-Pro.local>
2020-03-02 20:43:36 +0530

    rendering the text from future

--- lib/pages/samplehtml.dart
+++ lib/pages/samplehtml.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 class SampleHtml extends StatefulWidget {
@@ -11,35 +12,31 @@ class SampleHtml extends StatefulWidget {
 class _SampleHtmlState extends State<SampleHtml> {
   var _pageController = PageController();
 
+
   @override
   Widget build(BuildContext context) {
+    Future<String> loadAsset() async {
+      return await rootBundle.loadString('assets/webdriver.html');
+    }
+
     return Container(
       child: PageView(
         controller: _pageController,
         scrollDirection: Axis.vertical,
         children: <Widget>[
-          Scaffold(
-            body: HtmlWidget(
-              """
-<h1>Heading</h1>
-<p>A paragraph with <strong>strong</strong> <em>emphasized</em> text.</p>
-<ol>
-  <li>List item number one</li>
-  <li>
-    Two
-    <ul>
-      <li>2.1 (nested)</li>
-      <li>2.2</li>
-    </ul>
-  </li>
-  <li>Three</li>
-</ol>
-<p>And YouTube video!</p>
-<iframe src="https://www.youtube.com/embed/jNQXAC9IVRw" width="560" height="315"></iframe>
-""",
-              webView: true,
+         FutureBuilder(
+                future: loadAsset(),
+                builder: (context, snapshot){
+                  if(snapshot.data == null){
+                    return Container(
+                      child: Center(
+                        child: Text("Loading..."),
+                      ),
+                    );
+                  }
+                  return Text(snapshot.data);
+                },
             ),
-          ),
           Scaffold(
             body: Container(
               child: Center(child: Text("Page 2")),
